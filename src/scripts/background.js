3ebeb80c60c7b77b3653de3572ee639e2a8ca364
Stefan Van Damme <20127018+stefanvd@users.noreply.github.com>
2026-07-16 22:10:11 +0200

    Update background.js

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -377,36 +377,39 @@ if(exbrowser != "safari"){
 				chrome.action.setPopup({tabId: tab.id, popup:"popup.html"});
 			}else{
 				clickbutton += 1;
-				timer = setTimeout(async function(){
-					const thatpanellength = await getPopupOpenLength();
-					if(thatpanellength != 0){
-						// console.log("Doubleclick");
-						// console.log("yes popup open")
-						clickbutton = 0;
-						clearTimeout(timer);
-					}else{
-						// console.log("no popup open")
-						if(clickbutton == 1){
-							const chromeset = await chrome.storage.sync.get(["alllightsoff", "mousespotlights"]);
-							if((chromeset["mousespotlights"] != true)){ // regular lamp
-								if((chromeset["alllightsoff"] != true)){
-									chrome.scripting.executeScript({
-										target: {tabId: tab.id},
-										files: ["scripts/light.js"]
-									});
-								}else{
-									chrome.tabs.sendMessage(tab.id, {action: "masterclick"});
-								}
-							}else{ // all tabs
-								// Night Mode profile
-								// Eye Protection profile
-								chrome.tabs.sendMessage(tab.id, {action: "masterclick"});
+				timer = setTimeout(function(){
+					getPopupOpenLength().then((thatpanellength) => {
+						if(thatpanellength != 0){
+							// console.log("Doubleclick");
+							// console.log("yes popup open")
+							clickbutton = 0;
+							clearTimeout(timer);
+						}else{
+							// console.log("no popup open")
+							if(clickbutton == 1){
+								chrome.storage.sync.get(["alllightsoff", "mousespotlights"], function(chromeset){
+									if((chromeset["mousespotlights"] != true)){ // regular lamp
+										if((chromeset["alllightsoff"] != true)){
+											chrome.scripting.executeScript({
+												target: {tabId: tab.id},
+												files: ["scripts/light.js"]
+											});
+										}else{
+											chrome.tabs.sendMessage(tab.id, {action: "masterclick"});
+										}
+									}else{ // all tabs
+										// Night Mode profile
+										// Eye Protection profile
+										chrome.tabs.sendMessage(tab.id, {action: "masterclick"});
+									}
+								});
 							}
+							clickbutton = 0;
+							// Clear all timers
+							clearTimeout(timer);
 						}
-						clickbutton = 0;
-						// Clear all timers
-						clearTimeout(timer);
-					}
+					});
+					chrome.action.setPopup({tabId: tab.id, popup:""});
 				}, 250);
 				chrome.action.setPopup({tabId: tab.id, popup:"palette.html"});
 			}
@@ -432,24 +435,25 @@ if(exbrowser != "safari"){
 					setTimeout(() => { chrome.action.setPopup({tabId: tab.id, popup:""}); }, 500);
 				}
 
-				timer = setTimeout(async function(){
+				timer = setTimeout(function(){
 					// console.log("Singelclick");
 					if(clickbutton == 1){
-						const chromeset = await chrome.storage.sync.get(["alllightsoff", "mousespotlights"]);
-						if((chromeset["mousespotlights"] != true)){ // regular lamp
-							if((chromeset["alllightsoff"] != true)){
-								chrome.scripting.executeScript({
-									target: {tabId: tab.id},
-									files: ["scripts/light.js"]
-								});
-							}else{
+						chrome.storage.sync.get(["alllightsoff", "mousespotlights"], function(chromeset){
+							if((chromeset["mousespotlights"] != true)){ // regular lamp
+								if((chromeset["alllightsoff"] != true)){
+									chrome.scripting.executeScript({
+										target: {tabId: tab.id},
+										files: ["scripts/light.js"]
+									});
+								}else{
+									chrome.tabs.sendMessage(tab.id, {action: "masterclick"});
+								}
+							}else{ // all tabs
+								// Night Mode profile
+								// Eye Protection profile
 								chrome.tabs.sendMessage(tab.id, {action: "masterclick"});
 							}
-						}else{ // all tabs
-							// Night Mode profile
-							// Eye Protection profile
-							chrome.tabs.sendMessage(tab.id, {action: "masterclick"});
-						}
+						});
 					}
 					clickbutton = 0;
 					// Clear all timers
